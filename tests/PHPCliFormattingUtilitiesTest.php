1c3628b8890e641701e0ba4ece2fe1e48d0e09a4
Sevi Darling <sevidmusic@gmail.com>
2026-01-08 13:22:07 -0500

    Finished preparing for development.

---
+++ tests/PHPCliFormattingUtilitiesTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Darling\PHPCliFormattingUtilities\tests;
+
+use Darling\PHPUnitTestUtilities\traits\PHPUnitConfigurationTests;
+use Darling\PHPUnitTestUtilities\traits\PHPUnitRandomValues;
+use Darling\PHPUnitTestUtilities\traits\PHPUnitTestMessages;
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Defines common methods that may be useful to all roady test
+ * classes.
+ *
+ * All roady test classes must extend from this class.
+ *
+ * @internal
+ */
+#[CoversNothing]
+class PHPCliFormattingUtilitiesTest extends TestCase
+{
+    use PHPUnitConfigurationTests;
+    use PHPUnitRandomValues;
+    use PHPUnitTestMessages;
+}
